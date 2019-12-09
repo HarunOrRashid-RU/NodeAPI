1c04708f1bbd34f9123d0f3195a4bd26052a6567
Md Harun Or Rashid <horrashid007@gmail.com>
2019-12-09 22:59:26 +0600

    Add tour feature completely

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
+const tourRouter = require('./routes/tourRoute');
 const app = express();
 
 //Middleware
 app.use(express.json());
 
 //Route
-
+app.use('/api/v1/posts', tourRouter);
 
 //export app
 module.exports = app;
