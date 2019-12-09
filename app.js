d5d5b0100dd434a5f1df33ac8798b2a93d63c92f
Md.Harun Or Rashid <horrashid007@gmail.com>
2019-12-09 22:18:35 +0600

    API basic setup with MVC pattern

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+//Middleware
+app.use(express.json());
+
+//Route
+
+
+//export app
+module.exports = app;
---
+++ readme.md
@@ -0,0 +1,3 @@
+# NODE API Application
+
+Built using modern technologies: node.js, express, mongoDB and mongoose 🙂 🙂 🙂
